8a723574458f056b7bc78f5a99898faedc9ac514
Mav3r1ck <mav3r1ckmediastudio@gmail.com>
2026-06-24 22:42:38 +0100

    initial scaffold: svelte frontend + fastapi backend + maplibre

---
+++ frontend/src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { onMount } from 'svelte';
+  import maplibregl from 'maplibre-gl';
+  import 'maplibre-gl/dist/maplibre-gl.css';
+
+  let map;
+
+  onMount(() => {
+    map = new maplibregl.Map({
+      container: 'map',
+      style: 'https://api.maptiler.com/maps/dataviz-dark/style.json?key=7DkZfFXHsvdG3ZMivAV6',
+      center: [-2.89, 56.39],
+      zoom: 12
+    });
+  });
+</script>
+
+<div id="map"></div>
+
+<style>
+  #map {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+</style>
